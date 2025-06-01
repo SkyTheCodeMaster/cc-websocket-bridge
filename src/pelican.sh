48875d7e92961a6dc71e7ba3cb041da44807eb9e
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2025-06-01 13:03:02 -0400

    Add pelican setup file, hopefully it works

---
+++ src/pelican.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Config file for Pelican Egg
+# Python interpreter
+PYTHON="python3.13"
+MAIN_FILE="main.py"
+
+# Upgrade pip
+$PYTHON -m pip install -U pip
+
+# Install dependencies
+$PYTHON -m pip install -r requirements.txt
+
+# Generate run.sh
+cat <<END > run.sh
+#!/bin/bash
+
+$PYTHON $MAIN_FILE
+END
+
+# Make run.sh executable
+chmod +x run.sh
